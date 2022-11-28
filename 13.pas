1647c0ff3bd2d66a5957d4fe787b5e27e5f4dd7d
DanMitozz <117995535+DanMitozz@users.noreply.github.com>
2022-11-28 17:36:05 +0300

    Add files via upload

---
+++ 13.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var a:=readstring('Введите строку: ');
+  var b:=length(a);
+  for var i:=1 to b do begin
+    if (a[i] = 'a')or(a[i] = 'b')or(a[i] = 'c') then 
+      print('ok')
+    else print('Строка не содержит таких символов ''a,b,c '' ');
+     break
+     end; 
+end.
---
+++ 18.pas
@@ -0,0 +1,9 @@
+﻿begin 
+var s:=readstring('Введите строку с aba: ');
+var k1,k2,i:integer;
+ k1:=0;
+ k2:=0;
+ for  i:=1 to length(s) do
+if copy(s,i,3)='aba' then inc(k1);
+ write(k1);
+end.
